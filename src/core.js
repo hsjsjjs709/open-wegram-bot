e40eb9bc3634f74861d869baf5ee84dc4b233ab7
hsjsjjs709 <a13141516ee@proton.me>
2026-07-30 02:40:29 +0800

    更新 core.js

--- src/core.js
+++ src/core.js
@@ -109,7 +109,7 @@ export async function handleWebhook(request, ownerUid, botToken, secretToken) {
       }
       // ---------------------
 
-98    await postToTelegramApi(
+   await postToTelegramApi(
 
                 await postToTelegramApi(botToken, 'copyMessage', {
                     chat_id: parseInt(senderUid),
